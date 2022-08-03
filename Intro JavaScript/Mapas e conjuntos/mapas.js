f132f606b1941a7844ff20fdfdd587e014142c73
LuizBahl <luizbahl@hotmail.com>
2022-08-03 00:08:50 -0300

    Aulas de Metodos e Objetos JS

---
+++ Intro JavaScript/Mapas e conjuntos/mapas.js
@@ -0,0 +1,18 @@
+function getAdimins(map) {
+    let admins = [];
+    for([key, value] of map){
+        if(value === 'Admin') {
+            admins.push(key)
+        }
+    }
+    return admins
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz', "Admin")
+usuarios.set('Stephany', 'Admin');
+usuarios.set('Jorge', "User")
+usuarios.set('Natalia', 'Admin');
+
+console.log(getAdimins(usuarios))
